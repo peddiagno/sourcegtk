65c4b19964c6718a73f2941783d3a1631dedc9bc
Joao Paulo <j.santos@diagno.ind.br>
2019-03-26 11:33:59 -0300

    fonte da gtk

---
+++ gtk/inspector/misc-info.ui.h
@@ -0,0 +1,16 @@
+N_("State");
+N_("Buildable ID");
+N_("Default Widget");
+N_("Properties");
+N_("Focus Widget");
+N_("Properties");
+N_("Mnemonic Label");
+N_("Allocated size");
+N_("Clip area");
+N_("Accessible role");
+N_("Accessible name");
+N_("Accessible description");
+N_("Mapped");
+N_("Realized");
+N_("Is Toplevel");
+N_("Child Visible");
